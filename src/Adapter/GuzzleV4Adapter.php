debeb835f0830d6188e3d4fd89915108b96869ce
Marcos Sigueros <alrik11es@gmail.com>
2014-10-26 10:44:54 +0100

    Guzzle 4 and probably 5 support

---
+++ src/Adapter/GuzzleV4Adapter.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace DigitalOceanV2\Adapter;
+
+use DigitalOceanV2\Adapter\AbstractAdapter;
+use DigitalOceanV2\Adapter\AdapterInterface;
+use DigitalOceanV2\Exception\ExceptionInterface;
+use Guzzle\Http\Client;
+use Guzzle\Http\Message\Response;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Event\CompleteEvent;
+use GuzzleHttp\Message\ResponseInterface;
+
+class GuzzleV4Adapter extends AbstractAdapter implements AdapterInterface
+{
+    /**
+     * @var ClientInterface
+     */
+    protected $client;
+
+    /**
+     * @var Response
+     */
+    protected $response;
+
+    /**
+     * @var ExceptionInterface
+     */
+    protected $exception;
+
+    /**
+     * @param string             $accessToken
+     * @param ClientInterface    $client      (optional)
+     * @param ExceptionInterface $exception   (optional)
+     */
+    public function __construct($accessToken, ClientInterface $client = null, ExceptionInterface $exception = null)
+    {
+        $that            = $this;
+        $this->client    = $client ?: new \GuzzleHttp\Client();
+        $this->exception = $exception;
+
+        $this->client->setDefaultOption('headers/Authorization', sprintf('Bearer %s', $accessToken));
+
+        $this->client->getEmitter()->on('complete', function(CompleteEvent $e) use ($that){
+            $that->handleResponse($e);
+            $e->stopPropagation();
+        });
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function get($url)
+    {
+        $this->response = $this->client->get($url);
+
+        return $this->response->getBody(true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function delete($url, array $headers = array())
+    {
+        $this->response = $this->client->delete($url, $headers);
+
+        return $this->response->getBody(true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function put($url, array $headers = array(), $content = '')
+    {
+        $headers['content-type'] = 'application/json';
+        $request = $this->client->put($url, $headers, $content);
+        $this->response = $request;
+
+        return $this->response->getBody(true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function post($url, array $headers = array(), $content = '')
+    {
+        $headers['content-type'] = 'application/json';
+        $request = $this->client->post($url, $headers, $content);
+        $this->response = $request;
+
+        return $this->response->getBody(true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getLatestResponseHeaders()
+    {
+        if (null === $this->response) {
+            return null;
+        }
+
+        return array(
+            'reset'     => (integer) (string) $this->response->getHeader('RateLimit-Reset'),
+            'remaining' => (integer) (string) $this->response->getHeader('RateLimit-Remaining'),
+            'limit'     => (integer) (string) $this->response->getHeader('RateLimit-Limit'),
+        );
+    }
+
+    /**
+     * @param  CompleteEvent                                $event
+     * @throws \RuntimeException|ExceptionInterface
+     */
+    protected function handleResponse(CompleteEvent $event)
+    {
+        $this->response = $event->getResponse();
+
+        if ($this->response->getStatusCode() == '200') {
+            return;
+        }
+
+        $body = $this->response->getBody(true);
+        $code = $this->response->getStatusCode();
+
+        if ($this->exception) {
+            throw $this->exception->create($body, $code);
+        }
+
+        $content = json_decode($body);
+
+        throw new \RuntimeException(sprintf('[%d] %s (%s)', $code, $content->message, $content->id), $code);
+    }
+}
