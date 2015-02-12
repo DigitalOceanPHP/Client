467dbd89e1b530dbe69758e46f057dc01a9e5072
zCarot <zcarot@gmail.com>
2015-02-12 13:09:16 +0300

    Dynamic properties for all entities

--- src/Entity/AbstractEntity.php
+++ src/Entity/AbstractEntity.php
@@ -16,6 +16,8 @@ namespace DigitalOceanV2\Entity;
  */
 abstract class AbstractEntity
 {
+    protected $_unknownProperties = array();
+    
     /**
      * @param \stdClass|array $parameters
      */
@@ -34,10 +36,15 @@ abstract class AbstractEntity
     public function __get($property)
     {
         if (!property_exists($this, $property)) {
+            if (array_key_exists($property, $this->_unknownProperties)) {
+                 return $this->_unknownProperties[$property];
+            }
             throw new \InvalidArgumentException(sprintf(
                 'Property "%s::%s" does not exist.', get_class($this), $property)
             );
         }
+
+        
     }
 
     /**
@@ -49,9 +56,13 @@ abstract class AbstractEntity
     public function __set($property, $value)
     {
         if (!property_exists($this, $property)) {
+            $this->_unknownProperties[$property] = $value;
+            // TODO: Show warning for debug
+            /*
             throw new \InvalidArgumentException(sprintf(
                 'Property "%s::%s" should exist.', get_class($this), $property)
             );
+            */
         }
     }
 
