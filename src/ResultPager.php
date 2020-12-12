31963f6a785cd590b0e9e34efa8b6e9d762d1140
Graham Campbell <graham@alt-three.com>
2020-12-12 16:02:56 +0000

    Added missing file headers

--- src/ResultPager.php
+++ src/ResultPager.php
@@ -2,6 +2,16 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the DigitalOceanV2 library.
+ *
+ * (c) Antoine Corcy <contact@sbin.dk>
+ * (c) Graham Campbell <graham@alt-three.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace DigitalOceanV2;
 
 use Closure;
