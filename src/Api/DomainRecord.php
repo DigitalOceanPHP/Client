d6cd32d3e3151e3a4b7b1637b2b3d4a42fb11352
Lewis Smallwood <lewis@smallwood.it>
2021-01-25 13:41:46 +0000

    ISSUE-265 Alter code to pass static analysis

--- src/Api/DomainRecord.php
+++ src/Api/DomainRecord.php
@@ -84,7 +84,9 @@ class DomainRecord extends AbstractApi
 
             case 'NS':
                 $content = ['type' => $type, 'data' => $data];
-                if (is_null($name)) $name = "@";
+                if (null === $name) {
+                    $name = "@";
+                }
                 $content['name'] = $name;
 
                 break;
