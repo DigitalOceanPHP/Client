4863deb5bb7a135f2899ccbfac358103c4c112a0
Nicolas Beauvais <2951704+nicolasbeauvais@users.noreply.github.com>
2021-11-23 12:21:42 +0100

    Create a ResourceNotFoundException (#291)

---
+++ src/Exception/ResourceNotFoundException.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the DigitalOcean API library.
+ *
+ * (c) Antoine Corcy <contact@sbin.dk>
+ * (c) Graham Campbell <graham@alt-three.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace DigitalOceanV2\Exception;
+
+/**
+ * @author Graham Campbell <graham@alt-three.com>
+ */
+class ResourceNotFoundException extends RuntimeException
+{
+}
