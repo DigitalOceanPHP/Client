b06f2d884e838e00258768985bcf93ee5462664d
alkavan <igal@alkontek.com>
2025-03-11 16:23:08 +0200

    Refactoring the networks fix code to comply with phpstan rules.
    * Tested multiple solutions, this is the nicest and most minimal one, and probably more correct since we are not manipulating values we iterate on.
    * This also this resulted some cleanup on the baseline rules file.

--- src/Entity/Droplet.php
+++ src/Entity/Droplet.php
@@ -103,11 +103,16 @@ final class Droplet extends AbstractEntity
 
                         if (\property_exists($value, 'v6')) {
                             foreach ($value->v6 as $subValue) {
-                                $subValue->version = 6;
-                                $subValue->cidr = \is_int($subValue->netmask) ?
-                                    $subValue->ip_address.'/'.$subValue->netmask : $subValue->netmask;
-                                $subValue->netmask = null;
-                                $this->networks[] = new Network($subValue);
+                                /** @var object{ip_address: string, netmask: int|string, gateway: string, type: string} $subValue */
+                                $this->networks[] = new Network((object) [
+                                    'ip_address' => $subValue->ip_address,
+                                    'netmask' => null,
+                                    'gateway' => $subValue->gateway,
+                                    'type' => $subValue->type,
+                                    'cidr' => \is_int($subValue->netmask) ?
+                                        $subValue->ip_address.'/'.$subValue->netmask : $subValue->netmask,
+                                    'version' => 6,
+                                ]);
                             }
                         }
                     }
