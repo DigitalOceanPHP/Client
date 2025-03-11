70036b76d516e0e27a85f76dc8036b786d42fbb6
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2025-03-11 19:51:06 +0000

    IPv6 netmasks are always integers

--- src/Entity/Droplet.php
+++ src/Entity/Droplet.php
@@ -103,16 +103,13 @@ final class Droplet extends AbstractEntity
 
                         if (\property_exists($value, 'v6')) {
                             foreach ($value->v6 as $subValue) {
-                                /** @var object{ip_address: string, netmask: int|string, gateway: string, type: string} $subValue */
+                                /** @var object{ip_address: string, netmask: int, gateway: string, type: string} $subValue */
                                 $this->networks[] = new Network((object) [
                                     'ip_address' => $subValue->ip_address,
                                     'netmask' => null,
                                     'gateway' => $subValue->gateway,
                                     'type' => $subValue->type,
-                                    'cidr' => match (\is_int($subValue->netmask)) {
-                                        true => \sprintf('%s/%d', $subValue->ip_address, $subValue->netmask),
-                                        default => $subValue->netmask,
-                                    },
+                                    'cidr' => \sprintf('%s/%d', $subValue->ip_address, $subValue->netmask),
                                     'version' => 6,
                                 ]);
                             }
